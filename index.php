dcadc7583df0a1e6b875d83a42525acf52ac8646
João Eduardo Corner <joaoeduardocorner@gmail.com>
2024-05-23 13:35:00 -0300

    adicionando icone e logo

--- index.php
+++ index.php
@@ -5,10 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Título do Painel</title>
     <link rel="stylesheet" type="text/css" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <!--  Necessário para o funcionamento de forma responsiva  -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/icone.png">
+
+
 </head>
 <body>
     <div class='login'>
         <div class='form'>
+            <img src="img/logo.png" alt="" srcset="" class="imagem">
             <form>
                 <input type="email" name="usuario" placeholder="Digite seu E-mail">
                 <input type="password" name="senha" placeholder="Digite sua Senha">
