aa09a840163ece7b3439a6cb9e3bb17fb88a2bdd
João Eduardo Corner <joaoeduardocorner@gmail.com>
2024-05-25 20:54:21 -0300

    adicionado classe para esconder (responsivo) e botões de edição/exclusão

--- painel/paginas/usuarios/listar.php
+++ painel/paginas/usuarios/listar.php
@@ -39,13 +39,26 @@ for ($i = 0; $i < $linhas; $i++) {
     echo <<<HTML
 <tr>
 <td>{$nome}</td>
-<td>{$telefone}</td>
-<td>{$email}</td>
-<td>{$nivel}</td>
-<td></td>
+<td class="esc">{$telefone}</td>
+<td class="esc">{$email}</td>
+<td class="esc">{$nivel}</td>
+<td>
+
+<big><a href="#" onclick="editar('{$id}','{$nome}','{$email}','{$telefone}','{$endereco}','{$nivel}')" title="Editar Dados"><i class="fa fa-edit text-primary"></i></a></big>
+<li class="dropdown head-dpdn2" style="display: inline-block;">
+		<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><big><i class="fa fa-trash-o text-danger"></i></big></a>
+
+		<ul class="dropdown-menu" style="margin-left:-230px;">
+		<li>
+		<div class="notification_desc2">
+		<p>Confirmar Exclusão? <a href="#" onclick="excluir('{$id}')"><span class="text-danger">Sim</span></a></p>
+		</div>
+		</li>										
+		</ul>
+</li>
+</td>
 </tr> 
 HTML;
-
 }
 
 echo <<<HTML
