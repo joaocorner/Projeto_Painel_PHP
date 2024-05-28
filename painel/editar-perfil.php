82c8ee8feae9f40af5c7560ab7185db74538e87b
João Eduardo Corner <joaoeduardocorner@gmail.com>
2024-05-28 16:21:01 -0300

    validação e adicionando foto no servidor

--- painel/editar-perfil.php
+++ painel/editar-perfil.php
@@ -34,7 +34,50 @@ if (@count($res) > 0 and $id != $id_reg) {
     exit();
 }
 
-$query = $pdo->prepare("UPDATE $tabela SET nome = :nome, email = :email, telefone = :telefone, senha = :senha, senha_crip = '$senha_crip', endereco = :endereco WHERE id = '$id'");
+
+
+//validar troca da foto
+$query = $pdo->query("SELECT * FROM $tabela where id = '$id'");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$total_reg = @count($res);
+if($total_reg > 0){
+	$foto = $res[0]['foto'];
+}else{
+	$foto = 'sem-foto.jpg';
+}
+
+
+
+//SCRIPT PARA SUBIR FOTO NO SERVIDOR
+$nome_img = date('d-m-Y H:i:s') .'-'.@$_FILES['foto']['name'];
+// remove todos os caracteres ilegais por traço
+$nome_img = preg_replace('/[ :]+/' , '-' , $nome_img);
+
+$caminho = 'images/perfil/' .$nome_img;
+
+$imagem_temp = @$_FILES['foto']['tmp_name']; 
+
+if(@$_FILES['foto']['name'] != ""){
+	$ext = pathinfo($nome_img, PATHINFO_EXTENSION);   
+	if($ext == 'png' or $ext == 'jpg' or $ext == 'jpeg' or $ext == 'gif'){ 
+	
+			//EXCLUO A FOTO ANTERIOR
+			if($foto != "sem-foto.jpg"){
+				@unlink('images/perfil/'.$foto);
+			}
+
+			$foto = $nome_img;
+		
+		move_uploaded_file($imagem_temp, $caminho);
+	}else{
+		echo 'Extensão de Imagem não permitida!';
+		exit();
+	}
+}
+
+
+
+$query = $pdo->prepare("UPDATE $tabela SET nome = :nome, email = :email, telefone = :telefone, senha = :senha, senha_crip = '$senha_crip', endereco = :endereco, foto = '$foto' WHERE id = '$id'");
 
 $query->bindValue(':nome', $nome);
 $query->bindValue(':email', $email);
