f965962959bb67ce10a056dffbd53f819f67e865
João Eduardo Corner <joaoeduardocorner@gmail.com>
2024-05-29 14:40:55 -0300

    criado menu de acesso
    cópia feita a partir de grupo_acessos com algumas modificações

---
+++ painel/paginas/acessos.php
@@ -0,0 +1,93 @@
+<?php
+$pag = 'acessos';
+?>
+<a onclick="inserir()" type="button" class="btn btn-primary">
+    <span class="fa fa-plus"></span> Acesso</a>
+
+
+
+<li class="dropdown head-dpdn2" style="display: inline-block;">
+    <a href="#" data-toggle="dropdown" class="btn btn-danger dropdown-toggle" id="btn-deletar" style="display: none">
+        <span class="fa fa-trash"></span> Deletar</a>
+    <ul class="dropdown-menu">
+        <li>
+            <div class="notification_desc2">
+                <p>Excluir selecionados? <a href="#" onclick="excluirSel()"><span class="text-danger">Sim</span></a></p>
+            </div>
+        </li>
+    </ul>
+</li>
+
+<div class="bs-example widget-shadow" style="padding:15px" id="listar">
+
+</div>
+
+<script type='text/javascript'>
+    $(document).ready(function() {
+        $('#tabela').DataTable({
+            "language": {
+                //"url": "https://cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json"
+            }
+        });
+    });
+</script>
+
+<input type="hidden" id="ids">
+
+<!-- Modal Perfil -->
+<div class="modal fade" id="modalForm" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title" id="exampleModalLabel"><span id="titulo_inserir"></span></h4>
+                <button id="btn-fechar" type="button" class="close" data-dismiss="modal" aria-label="Close" style="margin-top: -25px">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <form id="form">
+                <div class="modal-body">
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label>Nome</label>
+                            <input type="text" class="form-control" id="nome" name="nome" placeholder="Seu Nome" required>
+                        </div>
+
+                        <div class="col-md-4">
+                            <label>Grupo</label>
+                            <select class="form-control" id="grupo" name="grupo" required>
+                                <option value="0">Sem Grupo</option>
+                                <?php
+                                $query = $pdo->query("SELECT * FROM grupo_acessos order by id asc");
+                                $res = $query->fetchAll(PDO::FETCH_ASSOC);
+                                $linhas = @count($res);
+                                if ($linhas > 0) {
+                                ?>
+                                    <option>Administrador</option>
+                                    <option>Comum</option>
+                                <?php   }  ?>
+                            </select>
+                        </div>
+
+                        <div class="col-md-4" style="margin-top: 22px">
+
+                            <button type="submit" class="btn btn-primary">Salvar</button>
+
+                        </div>
+
+                        <input type="hidden" class="form-control" id="id" name="id">
+
+                        <br>
+                        <small>
+                            <div id="mensagem" align="center"></div>
+                        </small>
+                    </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script type='text/javascript'>
+    var pag = '<?= $pag ?>'
+</script>
+<script src='js/ajax.js'></script>
