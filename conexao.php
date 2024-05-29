4bc6e2df5713e99a5ff84394cce78fe30402e04c
João Eduardo Corner <joaoeduardocorner@gmail.com>
2024-05-29 08:31:43 -0300

    corrigido id padrão de config e instagram

--- conexao.php
+++ conexao.php
@@ -25,13 +25,13 @@ $query = $pdo->query("SELECT * FROM config");
 $res = $query->fetchAll(PDO::FETCH_ASSOC);
 $linhas = @count($res);
 if ($linhas == 0) {
-    $pdo->query("INSERT INTO config SET nome = '$nome_sistema', email = '$email_sistema', telefone = '$telefone_sistema', logo = 'logo.png', logo_rel = 'logo.jpg', icone = 'icone.png' ");
+    $pdo->query("INSERT INTO config SET nome = '$nome_sistema', email = '$email_sistema', telefone = '$telefone_sistema', logo = 'logo.png', logo_rel = 'logo.jpg', icone = 'icone.png', id = 1");
 } else {
     $nome_sistema = $res[0]['nome'];
     $email_sistema = $res[0]['email'];
     $telefone_sistema = $res[0]['telefone'];
     $endereco_sistema = $res[0]['endereco'];
-    $instagram = $res[0]['instagram'];
+    $instagram_sistema = $res[0]['instagram'];
     $logo_sistema = $res[0]['logo'];
     $logo_rel = $res[0]['logo_rel'];
     $icone_sistema = $res[0]['icone'];
