5290d3a914d08ac25c04321d704a69367cefc790
João Eduardo Corner <joaoeduardocorner@gmail.com>
2024-05-29 14:39:10 -0300

    registros de grupo_acessos
    base utilizada de usuarios, removido alguns campos e alterado varios outros

---
+++ painel/paginas/grupo_acessos/salvar.php
@@ -0,0 +1,26 @@
+<?php
+$tabela = 'grupo_acessos';
+require_once('../../../conexao.php');
+
+$nome = $_POST['nome'];
+$id = $_POST['id'];
+
+//Validação de nome
+$query = $pdo->query("SELECT * FROM $tabela where nome = '$email'");
+$res = $query->fetchAll(PDO::FETCH_ASSOC);
+$id_reg = @$res[0]['id'];
+if (@count($res) > 0 and $id != $id_reg) {
+    echo 'Nome já Cadastrado';
+    exit();
+}
+
+
+if ($id == '') {
+    $query = $pdo->prepare("INSERT INTO $tabela SET nome = :nome");
+} else {
+    $query = $pdo->prepare("UPDATE $tabela SET nome = :nome WHERE id = '$id'");
+}
+$query->bindValue(':nome', $nome);
+$query->execute();
+
+echo 'Salvo com Sucesso';
