9624e9a3149d20beb2871e12a9dc6e735cb98bb7
João Eduardo Corner <joaoeduardocorner@gmail.com>
2024-05-31 16:27:07 -0300

    gestão de acesso implementado visualmente e com redirecionamento
    ainda é possível acessar sabendo-se a URL

--- painel/index.php
+++ painel/index.php
@@ -3,11 +3,15 @@
 require_once("../conexao.php");
 require_once("verificar.php");
 
+$pag_inicial = 'home';
+if (@$_SESSION['nivel'] != 'Administrador') {
+	require_once("verificar_permissoes.php");
+}
 
 if (@$_GET['pagina'] != "") {
 	$pagina = @$_GET['pagina'];
 } else {
-	$pagina = 'home';
+	$pagina = $pag_inicial;
 }
 
 $id_usuario = @$_SESSION['id'];
@@ -145,32 +149,32 @@ if ($linhas > 0) {
 					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 						<ul class="sidebar-menu">
 							<li class="header">MENU NAVEGAÇÃO</li>
-							<li class="treeview">
+							<li class="treeview <?php echo @$home ?> ">
 								<a href="index.php">
 									<i class="fa fa-dashboard"></i> <span>Home</span>
 								</a>
 							</li>
-							<li class="treeview">
+							<li class="treeview <?php echo @$menu_pessoas ?>">
 								<a href="#">
 									<i class="fa fa-users"></i>
 									<span>Pessoas</span>
 									<i class="fa fa-angle-left pull-right"></i>
 								</a>
 								<ul class="treeview-menu">
-									<li><a href="index.php?pagina=usuarios"><i class="fa fa-angle-right"></i> Usuários</a></li>
+									<li class="<?php echo @$usuarios ?>"><a href="index.php?pagina=usuarios"><i class="fa fa-angle-right"></i> Usuários</a></li>
 
 								</ul>
 							</li>
 
-							<li class="treeview">
+							<li class="treeview  <?php echo @$menu_cadastros ?>">
 								<a href="#">
 									<i class="fa fa-plus"></i>
 									<span>Cadastro</span>
 									<i class="fa fa-angle-left pull-right"></i>
 								</a>
 								<ul class="treeview-menu">
-									<li><a href="index.php?pagina=grupo_acessos"><i class="fa fa-angle-right"></i> Grupos</a></li>
-									<li><a href="index.php?pagina=acessos"><i class="fa fa-angle-right"></i> Acessos</a></li>
+									<li class="<?php echo @$grupo_acessos ?>"><a href="index.php?pagina=grupo_acessos"><i class="fa fa-angle-right"></i> Grupos</a></li>
+									<li class="<?php echo @$acessos ?>"><a href="index.php?pagina=acessos"><i class="fa fa-angle-right"></i> Acessos</a></li>
 
 								</ul>
 							</li>
@@ -264,7 +268,7 @@ if ($linhas > 0) {
 								</div>
 							</a>
 							<ul class="dropdown-menu drp-mnu">
-								<li> <a href="" data-toggle="modal" data-target="#modalConfig"><i class="fa fa-cog"></i> Configurações</a> </li>
+								<li class="<?php echo @$configuracoes ?>"> <a href="" data-toggle="modal" data-target="#modalConfig"><i class="fa fa-cog"></i> Configurações</a> </li>
 								<li> <a href="" data-toggle="modal" data-target="#modalPerfil"><i class="fa fa-user"></i> Perfil</a> </li>
 								<li> <a href="./logout.php"><i class="fa fa-sign-out"></i> Sair</a> </li>
 							</ul>
