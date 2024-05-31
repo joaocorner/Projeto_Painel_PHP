941a17bdf0accbe3f6b04281b07d9d814e0f005b
João Eduardo Corner <joaoeduardocorner@gmail.com>
2024-05-31 12:34:16 -0300

    acrescentado parametros em listar e formatação

--- painel/js/ajax.js
+++ painel/js/ajax.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   listar();
 });
 
-function listar() {
+function listar(p1, p2, p3, p4, p5, p6) {
   $.ajax({
     url: "paginas/" + pag + "/listar.php",
     method: "POST",
-    data: {},
+    data: { p1, p2, p3, p4, p5, p6 },
     dataType: "html",
 
     success: function (result) {
@@ -24,74 +24,65 @@ function inserir() {
 }
 
 $("#form").submit(function (event) {
-
   event.preventDefault();
   var formData = new FormData(this);
 
   $.ajax({
-      url: 'paginas/' + pag + "/salvar.php",
-      type: 'POST',
-      data: formData,
+    url: "paginas/" + pag + "/salvar.php",
+    type: "POST",
+    data: formData,
 
-      success: function (mensagem) {
-          $('#mensagem').text('');
-          $('#mensagem').removeClass()
-          if (mensagem.trim() == "Salvo com Sucesso") {
+    success: function (mensagem) {
+      $("#mensagem").text("");
+      $("#mensagem").removeClass();
+      if (mensagem.trim() == "Salvo com Sucesso") {
+        $("#btn-fechar").click();
+        listar();
+      } else {
+        $("#mensagem").addClass("text-danger");
+        $("#mensagem").text(mensagem);
+      }
+    },
 
-              $('#btn-fechar').click();
-              listar();          
-
-          } else {
-
-              $('#mensagem').addClass('text-danger')
-              $('#mensagem').text(mensagem)
-          }
-
-
-      },
-
-      cache: false,
-      contentType: false,
-      processData: false,
-
+    cache: false,
+    contentType: false,
+    processData: false,
   });
-
 });
 
-function excluir(id){
-  $('#mensagem-excluir').text('Excluindo...')
+function excluir(id) {
+  $("#mensagem-excluir").text("Excluindo...");
   $.ajax({
-      url: 'paginas/' + pag + "/excluir.php",
-      method: 'POST',
-      data: {id},
-      dataType: "html",
+    url: "paginas/" + pag + "/excluir.php",
+    method: "POST",
+    data: { id },
+    dataType: "html",
 
-      success:function(mensagem){
-          if (mensagem.trim() == "Excluído com Sucesso") {
-              listar();
-          } else {
-              $('#mensagem-excluir').addClass('text-danger')
-              $('#mensagem-excluir').text(mensagem)
-          }
+    success: function (mensagem) {
+      if (mensagem.trim() == "Excluído com Sucesso") {
+        listar();
+      } else {
+        $("#mensagem-excluir").addClass("text-danger");
+        $("#mensagem-excluir").text(mensagem);
       }
+    },
   });
 }
 
-function ativar(id, acao){
-    $.ajax({
-        url: 'paginas/' + pag + "/mudar-status.php",
-        method: 'POST',
-        data: {id, acao},
-        dataType: "html",
+function ativar(id, acao) {
+  $.ajax({
+    url: "paginas/" + pag + "/mudar-status.php",
+    method: "POST",
+    data: { id, acao },
+    dataType: "html",
 
-        success:function(mensagem){
-            if (mensagem.trim() == "Alterado com Sucesso") {
-                listar();
-            } else {
-                $('#mensagem-excluir').addClass('text-danger')
-                $('#mensagem-excluir').text(mensagem)
-            }
-        }
-    });
+    success: function (mensagem) {
+      if (mensagem.trim() == "Alterado com Sucesso") {
+        listar();
+      } else {
+        $("#mensagem-excluir").addClass("text-danger");
+        $("#mensagem-excluir").text(mensagem);
+      }
+    },
+  });
 }
-
